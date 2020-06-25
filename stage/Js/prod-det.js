c8d30219e5f02b44c1b1a68ba006a01f50bf518e
mohamed-Gamal-Nor <midogamal3339@gmail.com>
2020-06-25 10:23:58 +0200

    some edit

--- stage/Js/prod-det.js
+++ stage/Js/prod-det.js
@@ -13,4 +13,40 @@ if (document.title.split("-")[1] == "Product Deatails") {
         zoomPosition: "inner",
         cssClass: " mz-show-arrows",
     };
+
+    var slides = 3;
+    if (window.innerWidth <= 992) {
+        slides = 1;
+    }
+    var swiper = new Swiper(".swiper-container", {
+        slidesPerView: slides,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    // review code start
+    let listButton = document.querySelectorAll(
+        ".product-review .review-links li"
+    );
+    let view = document.querySelectorAll(".product-review .view div");
+    console.log(view);
+    listButton.forEach((libutton) => {
+        libutton.addEventListener("click", (e) => {
+            handleActive(e);
+            view.forEach((v) => {
+                v.classList.remove("active");
+            });
+            document.getElementById(e.target.dataset.review).classList.add("active");
+        });
+    });
 }
